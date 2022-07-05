0f2e29f3800a7146350ad26a59e4a7468d758d94
주효진 <hyojin_030@naver.com>
2022-07-05 22:34:58 +0900

    220705 - Passing Data (1)

--- PassingData/PassingData/DetailViewController.swift
+++ PassingData/PassingData/DetailViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    // 인스턴스가 생성되는 시점에 해당 변수도 생성됨
+    var someString = ""
+    
+    // 화면이 출력되는 시점(viewDidLoad)에 생성, 이전에 접근 시 nil
+    @IBOutlet weak var someLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        someLabel.text = someString
     }
 }
--- PassingData/PassingData/ViewController.swift
+++ PassingData/PassingData/ViewController.swift
@@ -1,5 +1,6 @@
 // PassingData
 // 1. instance property
+// 2. segue
 
 import UIKit
 
@@ -7,12 +8,16 @@ class ViewController: UIViewController {
 
     @IBAction func moveToDetail(_ sender: Any) {
         let detailVC = DetailViewController(nibName: "DetailViewController", bundle: nil)
+        
+
+        detailVC.someString = "test"
+        
         self.present(detailVC, animated: true, completion: nil)
+        
         
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
